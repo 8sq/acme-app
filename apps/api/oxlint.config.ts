d9fe146e2cb0a3159eb6eebed88fe9d9739ce62c
S Dermoumi <hi@sdrm.me>
2026-03-16 12:41:33 +0000

    Use oxlint per package

---
+++ apps/api/oxlint.config.ts
@@ -0,0 +1,4 @@
+import { defineConfig } from "oxlint";
+import baseConfig from "@ygoma/oxlint-config";
+
+export default defineConfig({ ...baseConfig });
---
+++ apps/web/oxlint.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "oxlint";
+import baseConfig from "@ygoma/oxlint-config";
+
+export default defineConfig({
+  ...baseConfig,
+});
