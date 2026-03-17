cb831ea0ca8676897d7b2f90991a349f28b14bcd
S Dermoumi <hi@sdrm.me>
2026-03-17 08:33:46 +0000

    Fix retrieving CfEnv from context instead of runtime

--- apps/web/server/api/[...].ts
+++ apps/web/server/api/[...].ts
@@ -1,17 +1,7 @@
 import { defineEventHandler } from "nitro/h3";
 import app from "@ygoma/api";
-import type { NitroEventContext } from "../nitro-context";
 
 export default defineEventHandler((event) => {
-  const context = event.context as NitroEventContext;
-  const env = context.cloudflare?.env ?? {};
-  console.log(
-    "Handling request",
-    event.req.url,
-    "with env",
-    env,
-    "and context",
-    context,
-  );
+  const env = event.runtime?.cloudflare?.env ?? {};
   return app.fetch(event.req, env);
 });
